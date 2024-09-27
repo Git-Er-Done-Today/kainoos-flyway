36c0f212a86ecaddbafa008bd43cb35edf541a73
Matthew Jordan <matthew.jordan@kainos.com>
2024-09-27 14:38:03 +0100

    Added Migration Script to insert sales employees

---
+++ src/main/resources/db/migration/V4__insert_sales_employees.sql
@@ -0,0 +1,9 @@
+INSERT INTO Employee (name, national_insurance_no, bank_account, salary) VALUES
+("Benji Agnew", "PL 22 34 54 B", "GB 31839 0923 9343 2394", 25000),
+("Delilah Martin", "PL 20 12 77 B", "GB 9872 4432 9663 2224", 30000);
+
+INSERT INTO Sales_Employee (employee_id) VALUES
+(1), (2);
+
+SELECT * FROM Employee;
+SELECT * FROM Sales_Employee;
