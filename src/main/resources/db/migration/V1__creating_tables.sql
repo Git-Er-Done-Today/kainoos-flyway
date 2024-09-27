7e09f4f29b06868da5df5050c5afff57b5627cf1
Matthew Jordan <matthew.jordan@kainos.com>
2024-09-27 10:37:28 +0100

    Added all relevant NOT NULL constraints

--- src/main/resources/db/migration/V1__creating_tables.sql
+++ src/main/resources/db/migration/V1__creating_tables.sql
@@ -1,45 +1,44 @@
 CREATE TABLE Employee (
-    employee_id INT AUTO_INCREMENT,
-    name varchar(60),
-    role varchar(40),
-    nation_insurance varchar(13),
-    bank_account varchar(40),
-    salary DECIMAL(10, 2),
+    employee_id INT AUTO_INCREMENT NOT NULL,
+    name varchar(60) NOT NULL,
+    role varchar(40) NOT NULL,
+    nation_insurance varchar(13) NOT NULL,
+    bank_account varchar(40) NOT NULL,
+    salary DECIMAL(10, 2) NOT NULL,
     PRIMARY KEY (employee_id)
 );
 
 CREATE TABLE `Client` (
-    client_id INT AUTO_INCREMENT,
-    name varchar(60),
-    address varchar(120),
-    phone_number varchar(20),
+    client_id INT AUTO_INCREMENT NOT NULL,
+    name varchar(60) NOT NULL,
+    address varchar(120) NOT NULL,
+    phone_number varchar(20) NOT NULL,
     PRIMARY KEY (client_id)
 );
 
 CREATE TABLE Delivery_Employee_Project (
-    delivery_employee_id INT,
-    project_id INT,
-    working_on_proj BOOLEAN,
+    delivery_employee_id INT NOT NULL,
+    project_id INT NOT NULL,
+    working_on_proj BOOLEAN NOT NULL,
     PRIMARY KEY(delivery_employee_id, working_on_proj)
 );
 
 CREATE TABLE Technology (
-    tech_id INT,
-    name varchar(60),
+    tech_id INT NOT NULL,
+    name varchar(60) NOT NULL,
     PRIMARY KEY (tech_id)
 );
 
-
 CREATE TABLE Project (
-    project_id INT AUTO_INCREMENT,
-    `name` varchar(60),
-    `value` DECIMAL(15,2),
-    client_id INT,
-    tech_lead_id INT,
-    sales_employee_id INT,
-    commission DECIMAL(10,2),
-    start_date DATE,
-    completion_date DATE,
+    project_id INT AUTO_INCREMENT NOT NULL,
+    `name` varchar(60) NOT NULL,
+    `value` DECIMAL(15,2) NOT NULL,
+    client_id INT NOT NULL,
+    tech_lead_id INT NOT NULL,
+    sales_employee_id INT NOT NULL,
+    commission DECIMAL(10,2) NOT NULL,
+    start_date DATE NOT NULL,
+    completion_date DATE NOT NULL,
     PRIMARY KEY (project_id),
     FOREIGN KEY (tech_lead_id) REFERENCES Employee(employee_id),
     FOREIGN KEY (sales_employee_id) REFERENCES Employee(employee_id),
@@ -47,8 +46,7 @@ CREATE TABLE Project (
 );
 
 CREATE TABLE Tech_Proj (
-    tech_id INT,
-    proj_id INT,
+    tech_id INT NOT NULL,
+    proj_id INT NOT NULL,
     PRIMARY KEY (tech_id, proj_id)
 );
-
