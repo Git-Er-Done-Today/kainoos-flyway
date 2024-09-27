25917689c3ef8b394d425f8438e9fed357ddea7c
aaron.millen <aaron.millen@kainos.com>
2024-09-27 14:52:18 +0100

    added files

--- src/main/resources/Queries/show_client_with_highest_value_projects.sql
+++ src/main/resources/Queries/show_client_with_highest_value_projects.sql
@@ -1,5 +1,5 @@
 Select Client.name as 'Client Name', SUM(Project.value) as 'Project Value'
 from Client
 join Project using (client_id)
-group by Client.name
+group by Client.id
 order by SUM(Project.value) DESC LIMIT 1;
