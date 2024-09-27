aba72e534acb41d805919d2ab617f47a3149e638
CharlotteMKainos <charlotte.mcknight@kainos.com>
2024-09-27 14:56:16 +0100

    removed limit on sql script

--- src/main/resources/db/migration/v10__client_avg_value_project.sql
+++ src/main/resources/db/migration/v10__client_avg_value_project.sql
@@ -2,4 +2,4 @@ Select c.name as 'Client Name', AVG(p.value) as 'Average Project Value'
 from Client c
 join Project as p using (client_id)
 group by c.name
-order by AVG(p.value) LIMIT 1;
+order by AVG(p.value);
